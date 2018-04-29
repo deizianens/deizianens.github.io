8265e592ab9bcebe9014b5dcdc56c174fbd8df48
Deiziane <deiziane_ns@hotmail.com>
2018-04-28 23:26:19 -0300

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,118 @@
+!function() {
+	'use strict';
+
+	var canvas = document.querySelector('#webgl');
+
+	// Scroll variables
+	var scroll = 0.0, velocity = 0.0, lastScroll = 0.0;
+
+	// Initialize REGL from a canvas element
+	var regl = createREGL({
+		canvas: canvas,
+		onDone: function(error, regl) {
+			if (error) { alert(error); }
+		}
+	});
+
+	// Loading a texture
+	var img = new Image();
+	img.src = 'img/img4.jpg';
+	img.onload = function() {
+		setTimeout(function() { document.body.classList.remove('loading');}, 1000);
+
+		// Create a REGL draw command
+		var draw = regl({
+			frag: document.querySelector('#fragmentShader').textContent,
+			vert: 'attribute vec2 position; void main() { gl_Position = vec4(3.0 * position, 0.0, 1.0); }',
+			attributes: { position: [-1, 0, 0, -1, 1, 1] },
+			count: 3,
+			uniforms: {
+				globaltime: regl.prop('globaltime'),
+				resolution: regl.prop('resolution'),
+				aspect: regl.prop('aspect'),
+				scroll: regl.prop('scroll'),
+				velocity: regl.prop('velocity'),
+				texture: regl.texture(img)
+			}
+		});
+
+		// Hook a callback to execute each frame
+		regl.frame(function(ctx) {
+
+			// Resize a canvas element with the aspect ratio (100vw, 100vh)
+			var aspect = canvas.scrollWidth / canvas.scrollHeight;
+			canvas.width = 1024 * aspect;
+			canvas.height = 1024;
+
+			// Scroll amount (0.0 to 1.0)
+			scroll = window.pageYOffset / (document.documentElement.scrollHeight - window.innerHeight);
+
+			// Scroll Velocity
+			// Inertia example:
+			// velocity = velocity * 0.99 + (scroll - lastScroll);
+			velocity = 0;
+
+			lastScroll = scroll;
+
+			// Clear the draw buffer
+			regl.clear({ color: [0, 0, 0, 0] });
+
+			// Execute a REGL draw command
+			draw({
+				globaltime: ctx.time,
+				resolution: [ctx.viewportWidth, ctx.viewportHeight],
+				aspect: aspect,
+				scroll: scroll,
+				velocity: velocity
+			});
+		});
+	};
+
+}();
+
+$(document).ready(function(){
+	setTimeout(function(){
+		document.getElementById("logo").className="show";
+	}, 2000);
+
+	setTimeout(function(){
+		document.getElementById("social").className="show-2";
+	}, 2500);
+});
+
+
+/*Timeline */
+(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
+
