1424f775534cb6a5d2000a01a4f7f694bf6f545c
Deiziane Silva <deiziane_ns@hotmail.com>
2023-02-17 16:39:12 -0300

    adds new cursos :zap:

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
-import { preloadFonts } from './utils.js';
-import { RepeatTextScrollFx } from './repeatTextScrollFx.js';
+import { RepeatTextScrollFx } from "./repeatTextScrollFx.js";
+import { Cursor } from "./cursor.js";
+import { Title } from "./title.js";
 
-// Preload images and fonts and remove loader
-// preloadFonts('fxx5dng').then(() => {
+const splitting = Splitting();
 
-// Apply the effect on these elements
-document.querySelectorAll('[data-text-rep]').forEach(textEl => {
-    new RepeatTextScrollFx(textEl);
+document.querySelectorAll("[data-text-rep]").forEach((textEl) => {
+  new RepeatTextScrollFx(textEl);
 });
 
-document.body.classList.remove('loading');
+document.body.classList.remove("loading");
 
-// });
+const cursor = new Cursor(document.querySelector(".cursor"));
+
+[...document.querySelectorAll("h2")].forEach((el) => new Title(el));
+[...document.querySelectorAll("h2")].forEach((link) => {
+  link.addEventListener("mouseenter", () => cursor.enter());
+  link.addEventListener("mouseleave", () => cursor.leave());
+});
